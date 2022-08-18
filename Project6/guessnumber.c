17e3714134328c0233735d8392eb385c4fc1b244
eunjin1012 <72156494+eunjin1012@users.noreply.github.com>
2022-08-18 16:50:36 +0900

    guessnumber

---
+++ Project6/guessnumber.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void)
+{
+	int answer = 59;
+	int guess;
+	int tries = 0;
+
+	do
+	{
+		printf("정답을 추측하여 보시오: ");
+		scanf("%d", &guess);
+
+		tries++;
+		if (answer > guess)
+			printf("제시한 정수가 낮습니다.\n");
+
+		if (answer < guess)
+			printf("제시한 정수가 높습니다.\n");
+
+	} while (guess != answer);
+	printf("축하합니다. 시도횟수=%d", tries);
+	return 0;
+}
